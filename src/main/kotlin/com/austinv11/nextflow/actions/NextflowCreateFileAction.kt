b916056dde5662d3ac6a897a1e48fce8d2550b9e
Austin Varela <austinv11@gmail.com>
2026-05-08 00:51:55 -0400

    Add new file templates

---
+++ src/main/kotlin/com/austinv11/nextflow/actions/NextflowCreateFileAction.kt
@@ -0,0 +1,32 @@
+package com.austinv11.nextflow.actions
+
+import com.austinv11.nextflow.NextflowIcons
+import com.intellij.ide.actions.CreateFileFromTemplateAction
+import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+
+class NextflowCreateFileAction : CreateFileFromTemplateAction(
+    "Nextflow File",
+    "Create new Nextflow file",
+    NextflowIcons.FILE
+), DumbAware {
+
+    override fun buildDialog(
+        project: Project,
+        directory: PsiDirectory,
+        builder: CreateFileFromTemplateDialog.Builder
+    ) {
+        builder
+            .setTitle("New Nextflow File")
+            .addKind("Script", NextflowIcons.FILE, "Nextflow Script")
+            .addKind("Process", NextflowIcons.FILE, "Nextflow Process")
+            .addKind("Config", NextflowIcons.FILE, "Nextflow Config")
+            .addKind("Test", NextflowIcons.FILE, "Nextflow Test")
+    }
+
+    override fun getActionName(directory: PsiDirectory?, newName: String, templateName: String?): String {
+        return "Create Nextflow File $newName"
+    }
+}
